d4e91c5c5e97243436ae235ae1533110c2669081
Documenter.jl <documenter@juliadocs.github.io>
2024-02-26 09:06:36 +0000

    build based on 6edc1dd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FletcherChecksums","category":"page"},{"location":"#FletcherChecksums","page":"Home","title":"FletcherChecksums","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FletcherChecksums.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FletcherChecksums]","category":"page"}]
+}
