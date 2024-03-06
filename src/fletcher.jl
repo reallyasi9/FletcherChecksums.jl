c4030151fbee5988e19050daae6fe4785b6fac41
--get <reallyasi9@users.noreply.github.com>
2024-03-06 16:44:08 -0500

    Fix fletcher_checksum documentation

--- src/fletcher.jl
+++ src/fletcher.jl
@@ -7,26 +7,25 @@
 """
     fletcher_checksum(T, data, init::T = zero(T), modulo::T = typemax(T), blocksize::Integer = 1)
 
-Compute a hash of `data` using Fletcher's checksum .
+Compute a hash of `data` using Fletcher's checksum.
 
-The BSD checksum computes a 16-bit checksum value by bit-rotating the checksum value from the previous step to the right by 1 bit, then adding the next value from `data` and repeating. Sums are allowed to overflow, which resets the sum to zero.
+Fletcher's checksum computes two values: `c0`, a running sum of values modulo some number, and `c1`, a running sum of `c0` values modulo some number. The two values are contatenated bitwise into a single unsigned integer.
 
-This function is fast but suffers from many collisions. Flaws to keep in mind are:
+This function is fast and good at detecting small errors. Flaws to keep in mind are:
 1. zero values at the beginning of `data` do not affect the checksum value at all (unless the initial value is set to something other than zero); and
-2. runs of zeros in `data` that are multiples of 16 in length do not affect the checksum value at all.
+2. the worst-case performance for this algorithm occurs when data are truly random, but all errors of up to 2 bits are detected on all runs of data of length `modulo * (sizeof(T)*4)` bits.
 
 ## Arguments
 - `T`: An unsinged integer type.
 - `data`: A single `Unsigned` value or an iterator of values.
 - `init::Unsigned = zero(T)`: Optional starting value.
 - `modulo::T = typemax(T)`: Optional modulo value, applied independently to the sum and the sum-of-sums after each block is summed.
-- `blocksize::Integer = 1`: Optional block size for summing before applying the modulo operation.
+- `blocksize::Integer = 1`: Optional block size for summing before applying the modulo operation. `blocksize` should be chosen with `modulo`, `T`, and `eltype(data)` in mind to make sure the sum operation does not overflow.
 
 Predefined convenience functions are `fletcher16`, `fletcher32`, and `fletcher64` for the standard modulo value of `typemax(T)` shifted right half the number of bits in the checksum, and `fletcher16a`, `fletcher32a`, and `fletcher64a` for the alternate modulo value of `1` left shifted half the number of bits in the checksum.
 
 See also: [adler32](@ref).
 """
-
 function fletcher_checksum(::Type{T}, data, init::Unsigned = zero(T), modulo::T = typemax(T), blocksize::Integer = 1) where {T <: Unsigned}
     len = length(data)
     len == 0 && return init % T
@@ -69,4 +68,11 @@ fletcher32a(data, init::Unsigned = zero(UInt32)) = fletcher_checksum(UInt32, dat
 fletcher64a(data, init::Unsigned = zero(UInt64)) = fletcher_checksum(UInt64, data, UInt64(init), 0x0000000100000000, 92681)
 
 # adler32 is not optimized like the zlib version, but on modern hardware the Julia version has a runtime of anywhere between 90% and 110% of the zlib version.
+"""
+    adler32(data, init::Unsigned = one(UInt32))
+
+Compute Adler's 32-bit checksum.
+
+Adler's 32-bit checksum is the same as Fletcher's 32-bit checksum with a modulus of 65521 (the largest 16-bit prime) and an inital value of one. This is a strict improvement on `fletcher16`, but has a higher probability of collision, worse likelihood of error detection, and is slower than `fletcher32`. Prefer `fletcher32` instead.
+"""
 adler32(data, init::Unsigned = one(UInt32)) = fletcher_checksum(UInt32, data, UInt32(init), UInt32(65521), 5552)
